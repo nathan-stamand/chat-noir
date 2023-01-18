a48c509eae3045e6d26bf226fdc5e6c60cb8170a
Nathan St.Amand <nathansaint810@gmail.com>
2023-01-17 21:57:47 -0600

    Build frontend
    Add tailwind config file
    Build index.html and chat.html
    Handle socket events on frontend
    Handle basic JS on frontend
    Light organizing within JS files

---
+++ public/dist/js/index.js
@@ -0,0 +1,53 @@
+const params = createParamObject();
+const user = getUserInfo()["username"];
+const room = getUserInfo()["room"];
+
+if (params['error']) {
+  showError(params['error'], user, room);
+}
+
+function varExists(variable) {
+  return typeof variable !== "undefined";
+}
+
+function showError(code, user, room) {
+  if (!varExists(user) || !varExists(room)) {
+    console.error("User and room not set.")
+    return;
+  }
+  const errorCodes = {
+    "incomplete-fields": "Please complete both the username and the room field.",
+    "duplicate-username": `Username '${user}' already exists in room ${room}.`,
+    "invalid-username": `Username '${user}' is invalid, please choose another.`
+  };
+  if (typeof errorCodes[code] === "undefined") {
+    console.error(`No matching error code found for ${code}.`);
+    return;
+  }
+  const errorBox = document.querySelector("#error-box");
+  errorBox.innerHTML = errorCodes[code];
+  errorBox.style.display = "block";
+}
+
+function createParamObject() {
+  const errorParam = location.search.replace("?", "").split("&");
+  const params = {};
+  errorParam.forEach(param => {
+    let k = param.split("=")[0]
+    let v = param.split("=")[1]
+    params[k] = v
+  });
+  return params;
+}
+
+// TODO: Create helpers file, this is duplicate function
+function getUserInfo() {
+  const rawCookies = document.cookie.split("; ");
+  const cookies = {};
+  rawCookies.forEach(cookie => {
+    let k = cookie.split('=', 2)[0];
+    let v = cookie.split('=', 2)[1];
+    cookies[k] = v;
+  });
+  return cookies;
+}
