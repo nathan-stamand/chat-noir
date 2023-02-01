8a716c7e32db1a03a406a963e27a4369ec8976b7
Nathan St.Amand <nathansaint810@gmail.com>
2023-01-31 20:52:12 -0600

    Add functions for users exiting rooms (#9)

--- socket.js
+++ socket.js
@@ -7,6 +7,8 @@ function socket(server) {
   io.on('connect', (socket) => {
     console.log(':::sockets connected:::');
     socket.on('join-room', msg => {
+      socket.username = msg.username;
+      socket.room = msg.room;
       socket.join(msg.room);
       socket.broadcast.to(msg.room).emit('chat-message', {
         username: "System",
@@ -23,8 +25,36 @@ function socket(server) {
       })
     });
 
+    socket.on('disconnect', () => {
+      Room.findOne({name: socket.room}).then(room => {
+        var users = room.users.filter(user => user.name != socket.username)
+
+        if (users.length) {
+          return Room.updateOne({_id: room._id}, {users: users})
+        } else {
+          return Room.deleteOne({_id: room._id});
+        }
+      }).then(res => {
+        console.log(res)
+        if (res.modifiedCount == 1) {
+          console.log(`${socket.username} has left ${socket.room}`)
+        }
+      }).then(_ => {
+        socket.broadcast.to(socket.room).emit('chat-message', {
+          username: "System",
+          text: `${socket.username} has left ${socket.room}`,
+          timestamp: createTimeStamp(),
+        });
+
+        socket.broadcast.to(socket.room).emit('update-room')
+      });
+    });
+
     socket.on('chat-message', msg => {
       msg.timestamp = createTimeStamp();
+      Room.findOne({name: msg.room}).then(room => {
+        return Room.updateOne({_id: room._id}, { messages: [...room.messages, msg] })
+      })
       io.to(msg.room).emit('chat-message', msg)
     });
   });
